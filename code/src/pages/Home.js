101985fbd826cabe211bc15fbe60dc34e7ec7620
Harman Bhogal <bhogalharman7@gmail.com>
2025-03-17 22:56:35 -0700

    added nav bar matching workouts page

--- code/src/pages/Home.js
+++ code/src/pages/Home.js
@@ -8,30 +8,32 @@ import Button from './Button';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <h1>GitFit</h1>
+    <>
+      <div className="workoutsContainer">
+        <h1 id="title">GitFit</h1>
+        <hr />
 
-      <div className="dashboardButtonContainer">
-        <nav>
-          <ul>
-            {/* Links to workout page and dashboard pages, needs styling and button components */}
-            <li><Link to="/"><Button name="Dashboard"/></Link></li>
-            <li><Link to="/about"><Button name="Workouts"/></Link></li>
-          </ul>
-        </nav>
+        <div className="dashboardButtonContainer">
+          <nav>
+            <ul>
+              <li><Link to="/"><Button name="Dashboard" /></Link></li>
+              <li><Link to="/about"><Button name="Workouts" /></Link></li>
+            </ul>
+          </nav>
+        </div>
+        <hr />
       </div>
-      
-      <div className="step-circle-container">
-        {}
-        <SemicircleProgressBar goal={2000} text="Steps" /> 
-        {/*change steps and goal for steps input*/}
+
+      <div className="home-container">
+        <div className="step-circle-container">
+          <SemicircleProgressBar goal={2000} text="Steps" />
+        </div>
+        <div className="step-graph-container">
+          <h2>Weekly Steps</h2>
+          <StepChart />
+        </div>
       </div>
-      <div className="step-graph-container">
-        <h2>Weekly Steps</h2>
-        <StepChart />
-      </div>
-    </div>
-    
+    </>
   );
 };
 

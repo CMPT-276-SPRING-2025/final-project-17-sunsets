1ae5c37135e69664a3c973bf803f9581cf560bbd
Gurjot <jot.mahay@gmail.com>
2025-03-26 17:41:55 -0700

    Imported useState hook and began adding API functionality to searchbar

--- code/src/SearchBar.js
+++ code/src/SearchBar.js
@@ -1,12 +1,18 @@
-import React from "react-router-dom"
+import React, {useState} from "react"
 import {FaSearch} from "react-icons/fa"
 import "./SearchBar.css"
 
 const SearchBar = () => {
+    const [input, setInput] = useState("") //Create a stateful variable to hold user data
+
+    const fetchData = (value) => (
+        fetch("https://wger.de/api/v2/exercise/")
+    )
+
     return( 
     <div className="input-wrapper">
         <FaSearch id="search-icon" />
-        <input placeholder="Search Exercise" />
+        <input placeholder="Search Exercise" value={input} onChange={(e) => setInput(e.target.value)} />
     </div>
     )
 };

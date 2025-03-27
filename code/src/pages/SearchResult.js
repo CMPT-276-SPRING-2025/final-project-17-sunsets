8766c6bce2c70c32930132fe9d856f2e0f114e2a
Gurjot <jot.mahay@gmail.com>
2025-03-26 19:22:36 -0700

    Added CSS styling to Search Elements

--- code/src/pages/SearchResult.js
+++ code/src/pages/SearchResult.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import "./SearchResult.css"
 
 export const SearchResults = ({result}) => {
     return (
-        <div>{result.alias}</div>
+        <div className="search-result" onClick={(e) => alert(`You clicked on ${result.alias}`)}>{result.alias}</div>
     )    
 }

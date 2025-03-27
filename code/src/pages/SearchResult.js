5b6fa42b46acd234851085f96a94143266a39c2e
Gurjot <jot.mahay@gmail.com>
2025-03-26 19:09:32 -0700

    Added component to store search results

---
+++ code/src/pages/SearchResult.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const SearchResults = ({result}) => {
+    return (
+        <div>{result.alias}</div>
+    )    
+}
--- code/src/pages/SearchResultsList.js
+++ code/src/pages/SearchResultsList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./SearchResultsList.css"
+import { SearchResults } from './SearchResult'
 
 export const SearchResultsList = ({results}) => {
     return (
     <div className="results-list">
             {
                 results.map((result, id) => {
-                    return <div key={id}>{result.alias}</div>
+                    return <SearchResults result={result} key={id}/>
                 })
             }
 

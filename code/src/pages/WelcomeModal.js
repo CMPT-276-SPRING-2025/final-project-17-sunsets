ba77d24668c8f52f983a48b6048b66247de4d580
JagrajG <jagrajgill83@gmail.com>
2025-04-02 09:47:07 -0700

    updated welcome modal to be more specific

--- code/src/pages/WelcomeModal.js
+++ code/src/pages/WelcomeModal.js
@@ -9,13 +9,13 @@ const WelcomeModal = ({ onClose }) => {
 
         <div>
           <h3>1. Add your Info</h3>
-          <p>Navigate to the user icon and enter your info</p>
+          <p>Navigate to the user icon on the left han site and enter your information their</p>
 
           <h3>2. Set Your Goals</h3>
           <p>In your profile section you can update the amount of steps you'd like to walk daily</p>
 
           <h3>3. Make your workouts</h3>
-          <p>Navigate to the workouts page and create your own catered workout</p>
+          <p>Navigate to the workouts page and create a workout catered to your needs</p>
 
           <h3>4. Add your local weather</h3>
           <p>Navigate to the weathers page and update your location to get local weather based workouts</p>

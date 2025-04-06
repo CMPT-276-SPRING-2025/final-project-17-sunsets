e7e65e005b1399a376ed3cd8fd7cc2c2cf5ef00d
JagrajG <jagrajgill83@gmail.com>
2025-04-06 12:38:50 -0700

    fixed merge conflicts

--- code/src/pages/SearchResultsList.js
+++ code/src/pages/SearchResultsList.js
@@ -1,13 +1,7 @@
 import React from 'react'
 import "./SearchResultsList.css"
 import { SearchResults } from './SearchResult.js'
-
-<<<<<<< HEAD
 export const SearchResultsList = ({results, updateExerciseName}) => {
-=======
-//components to render a list of search results
-export const SearchResultsList = ({results}) => {
->>>>>>> formating_code
     return (
     //conntainer div for list of results
     <div className="results-list">

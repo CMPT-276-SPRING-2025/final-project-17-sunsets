30f3ba7fb63bb302e15fd4ffaeaefb4918e3f190
Jagraj Gill <36524289+JagrajG@users.noreply.github.com>
2025-05-01 22:12:41 -0700

    Update openWeatherAPI.test.js
    Removed api key

--- code/src/__tests__/openWeatherAPI.test.js
+++ code/src/__tests__/openWeatherAPI.test.js
@@ -1,6 +1,6 @@
 describe('OpenWeatherMap API', () => {
     it('fetches real weather data from OpenWeatherMap', async () => {
-      const API_KEY = '25124b238c6dc448050f80e9d16ace91'; // hard coded because I canont use github secrets 
+      const API_KEY = ''; // hard coded because I canont use github secrets 
       const city = 'Vancouver';
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   
